6e14849ceba721cbdb7446e4ff74b4c09e03a738
Taisen Colcher <TaisenColcher@Gmail.com>
2023-05-31 23:54:23 -0400

    Add PlayerMoveState Class

---
+++ Assets/Scripts/State Machine/PlayerMoveState.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveState : PlayerBaseState
+{
+
+    private readonly int MoveSpeedHash = Animator.StringToHash("MoveSpeed");
+
+    public PlayerMoveState(PlayerStateMachine stateMachine) : base(stateMachine) { }
+
+    public override void Enter()
+    {
+        stateMachine.velocity.y = Physics.gravity.y;
+
+        stateMachine.inputReader.OnJumpPerformed += SwitchToJumpState;
+    }
+
+    public override void Tick()
+    {
+        if(!stateMachine.characterController.isGrounded)
+        {
+            // stateMachine.SwitchState(new PlayerFallState(stateMachine));
+
+            CalculateMoveDirection();
+            FaceMoveDirection();
+            Move();
+        }
+    }
+
+    public override void Exit()
+    {
+        stateMachine.inputReader.OnJumpPerformed -= SwitchToJumpState;
+    }
+
+    private void SwitchToJumpState()
+    {
+        // stateMachine.SwitchState(new PlayerJumpState(stateMachine));
+    }
+
+
+
+}
